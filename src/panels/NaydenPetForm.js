f58b65cd014c9d0c7014125a6fb5929fc21a696a
korotkovdmitriy <dima_korotkov_@mail.ru>
2024-01-16 09:19:01 +0300

    фикс

--- src/panels/NaydenPetForm.js
+++ src/panels/NaydenPetForm.js
@@ -1,47 +1,384 @@
 
-function NaydenPetForm(){
-	return(
+import bridge from '@vkontakte/vk-bridge';
+import { RadioGroup, Radio, Group, FormItem, Input, Checkbox, Select, CustomSelectOption, Div, Button, Header, Card,PanelHeader, Link, CardGrid} from '@vkontakte/vkui';
+import React, { useEffect, useState, Image  } from "react";
+import { connect } from 'react-redux'
+import { infoMethod } from "../function/infoMethod"
+import { infoParam } from "../function/infoParam"
+import renderjson from "renderjson"
+import "../css/pet.css"
+function NaydenPetForm({router}){
+	const [falseDogName, setSfalseDogName] = useState(true)
+	const [falseDogDate, setSfalseDogDate] = useState(true)
+
+	const [section, setSection] = useState(null)
+	const [returnApi, setreturnApi] = useState(null)
+	const [use_method, setUseMethod] = useState(false)
+	const [disabledButtonMethod, setDisabledButtonMethod] = useState(false)
+	const [textButtonMethod, setTextButtonMethod] = useState(true)
+	const [param, setParam] = useState(
+		[
+		])
+//https://user573770298-pnrszkcg.wormhole.vk-apps.com/
+	useEffect(() => {
+		setParam([
+
+		])
+	}, [])
+
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms))
+	}
+
+	async function onChange(index) {
+		setUseMethod(false)
+		setTextButtonMethod(true)
+		setDisabledButtonMethod(false)
+
+		let arr = param
+
+		if (arr[index.currentTarget.name] === '') {
+			arr[index.currentTarget.name] = index.currentTarget.value
+		} else {
+			arr[index.currentTarget.name] = ""
+		}
+
+		setParam(arr)
+
+	}
+	async function onChangeInputsDog(e) {
+		const { value } = e.currentTarget;
+
+		let arr = param
+		arr[e.currentTarget.name] = e.currentTarget.value
+		setParam(arr)
+		sleep(1)
+		if (e.currentTarget.name === "dogName") {
+			setSection(e.currentTarget.value)
+			setSfalseDogName(true)
+
+		} else {
+			setSection(null)
+			//setSfalseDogName(false)
+
+		}
+
+		console.log(arr)
+
+		setUseMethod(false)
+		setTextButtonMethod(true)
+		setDisabledButtonMethod(false)
+	}
+
+	async function onChangeInputs(e) {
+		const { value } = e.currentTarget;
+
+		let arr = param
+		arr[e.currentTarget.name] = e.currentTarget.value
+		setParam(arr)
+		sleep(1)
+		setUseMethod(false)
+		setTextButtonMethod(true)
+		setDisabledButtonMethod(false)
+	}
+
+	function actionCheckbox(index) {
+
+		setUseMethod(false)
+		setTextButtonMethod(true)
+		setDisabledButtonMethod(false)
+		let arr = param
+		console.log(param.includes(index.currentTarget.name))
+
+
+		if (arr[index.currentTarget.name] === '' || arr[index.currentTarget.name] === true) {
+			arr[index.currentTarget.name] = false
+		} else {
+			arr[index.currentTarget.name] = true
+		}
+
+		setParam(arr)
+	}
+
+	function actionRadio(index) {
+
+		setUseMethod(false)
+		setTextButtonMethod(true)
+		setDisabledButtonMethod(false)
+		let arr = param
+		console.log(param.includes(index.currentTarget.name))
+		arr[index.currentTarget.name] = index.currentTarget.value
+		setParam(arr)
+	}
+
+	function addBlock(el) {
+		return (
+			<FormItem top={el.copy_history.text}>
+
+			</FormItem>
+		)
+	}
+
+
+	async function executeMethod() {
+		renderjson.set_show_to_level(30)
+		let params1 = {}
+		console.log(param["datePet"])
+		if (param["dogName"] === "" || param["dogName"] == null) {
+			setSfalseDogName(false)
+			return
+		}
+		if (param["datePet"] === "" || param["datePet"] == null) {
+			setSfalseDogDate(false)
+			return
+		}
+		try {
+			infoParam.map((el, index) => {
+				if (param[el] === '' || el.length < 2) { }
+				else {
+					params1[`${el}`] = param[el]
+
+				}
+				params1["type1"] = "#найдена"
+				params1["v"] = "5.131"
+				params1["access_token"] = "56412b1556412b1556412b15cb55500bcd5564156412b153510140686208a50ef42d93d"
+
+
+
+			})
+
+			let response = await bridge.send("VKWebAppCallAPIMethod", { "method": "execute.getMessagPet", "params": params1 });
+
+			window.responseApi = response
+			let items = response.response.items
+			setreturnApi(items)
+			console.log(response)
+
+		} catch {
+			//document.getElementById('response').appendChild("Error")
+		}
+
+
+	}
+
+
+	return (
 		<div>
-<h1>Сообщить о пропаже</h1>
-<div>
-<label className="datePropLabel">Введите дату пропажи:</label><br/>
-<input className='datePropVal'></input>
 
-</div>
-<div>
-<label className="textName">Выбирите породу:</label><br/>
-<select>
-<option value="1">Не могу определить</option>
-	<option value="2">Хаски</option>
-	<option value="3">Лобрадор</option>
-	<option value="4">Тресущая головой</option>
-</select>
-</div>
-<div>
-<label className="polPropLabel">Выбирете пол пропавшей собаки:</label><br/>
-<input className='polPropValM' id="polPropValM" type="radio" 
-     name="pol"></input>
-	 <label htmlFor="polPropValM">Кобель</label><br/>
-	 <input className='polPropValG' id="polPropValG" type="radio" 
-     name="pol"></input>
-	 <label htmlFor="polPropValG">Сучка</label>
+			<div>
+
+				<Group>
+					<FormItem top="Сообщить о находке">
+						<FormItem top="*Введите дату обнаружения:" bottom={falseDogDate ? "" : "поле обязательно для заполнения"}>
+							<Input
+								type="date"
+								name='datePet'
+								defaultValue=""
+								//max={Now()}
+								onChange={(e) => onChangeInputs(e)}
+								placeholder="Enter your name"
+								size={60}
+							/>
+
+						</FormItem>
+						<FormItem top="Выбирете пол найденой собаки:">
+							<RadioGroup>
+								<Radio name="polPropValG" value="m" onChange={(e) => actionRadio(e)} >
+									Мальчик
+								</Radio>
+								<Radio name="polPropValG" value="g" onChange={(e) => actionRadio(e)}>
+									Девочка
+								</Radio>
+
+							</RadioGroup>
+						</FormItem>
+    
+						<Group>
+							<FormItem className='petFromItem' top="*Выбирите породу найденой собаки из списка. Если ее не нашли в списке, выбирите 'моей породы нет в списке' и впишите ее руками в дополнительное поле
+		Если вы не знаете породу или у собаки ее нет, то выбирите 'не могу определить породу'"
+								bottom={falseDogName ? "" : "поле обязательно для заполнения"}>
+								<Select
+									name='dogName'
+									placeholder=''
+									options={infoMethod.map((el, index) => ({
+										label: el.title,
+										value: el.title,
+
+									}))}
+									renderOption={({ option, ...restProps }) => (
+										<CustomSelectOption
+											{...restProps}
+										/>
+									)}
+									onChange={(e) => onChangeInputsDog(e)}
+									searchable={true}
+								/>
 
 
-</div>
-<div>
-<label className="colorPropLabel">Окрас:</label><br/>
-<input className='colorPropVal'></input>
+							</FormItem>
+							{section !== null &&
+								<>
+									{infoParam.map((el, index) => {
+										if (el === 'dogName') {
+											console.log(param[el])
+											if (param[el] === "моей породы нет в списке") {
+												//console.log("start")
+												return (
+													<FormItem top="*Введите наименование породы" >
+														<Input
+															name='dogNameManual'
+															type='text'
+															onChange={(e) => onChangeInputs(e)}
 
-</div>
+														/>
+													</FormItem>
+												)
+											}
+										}
+									}
+									)}
+								</>}
+						</Group>
+						<FormItem className='petFromItem' top="Выбирете цвета присутствующие у найденому питомцу или преобладающий (можно выбрать несколько):">
+							<Checkbox
+								name='chern'
+								value={'черный'}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								черный
+							</Checkbox>
 
-<div>
-<label className="primetPropLabel">Приметы:</label><br/>
-<input className='porimetPropVal'></input>
+							<Checkbox
+								name='beliy'
+								value={'белый'}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								белый
+							</Checkbox>
+							<Checkbox
+								name='rihiy'
+								value={'рыжий'}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								рыжий
+							</Checkbox>
+							<Checkbox
+								name='seriy'
+								value={'бежсерыйевый'}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								серый
+							</Checkbox>
+							<Checkbox
+								name='begeviy'
+								value={'бежевый'}
+								//checked={disabled4}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								бежевый
+							</Checkbox>
+							<Checkbox
+								name='korih'
+								value={'коричневый'}
+								onChange={(e) => actionCheckbox(e)}
+							>
+								коричневый
+							</Checkbox>
+						</FormItem>
 
-</div>
-</div>
+						<Div>
+							<Button
+								size='1'
+								stretched
+								mode='secondary'
+								onClick={() => executeMethod()}
+							>Поиск</Button>
+						</Div>
+					</FormItem>
+				</Group>
+
+
+				<Group header={<Header mode='secondary'>Результат поиска</Header>}>
+					<Card>
+						<Div>
+							{returnApi !== null &&
+								<>
+
+									{returnApi.map((el) => {
+										//let polPet = {param["polPropValG"]==="m" ? 'мальчик' : 'девочка'}
+										//console.log(param["dogName"]!=="") date
+										//console.log(param["dogName"]!=null)
+
+
+										if (el.copy_history[0].text !== "" && param["dogName"] !== "" && param["dogName"] != null) {
+
+											if (el.text.toUpperCase().includes(param["dogName"].toUpperCase())) {
+												//console.log(el.date.getFullYear()+'-'+(el.date.getMonth()+1)+'-'+el.date.getDate()+' '+el.date.getHours()+':'+el.date.getMinutes()+':'+el.date.getSeconds()+'.'+el.date.getMilliseconds())
+												//console.log(new Date(el.date * 1000).toDateString())
+												//console.log(Math.floor(new Date(param["datePet"]).getTime() / 1000))
+												if (el.date * 1 > Math.floor(new Date(param["datePet"]).getTime() / 1000)) {
+													return (
+														<Group description={el.copy_history[0].text} key={el.id}>
+														<CardGrid size="s">
+															
+																
+
+															
+																	{el.copy_history[0].attachments.map((fotoMap) =>{
+																		console.log( fotoMap.type)
+																		
+																		if (fotoMap.type ==="photo") {
+																			console.log(fotoMap.photo.album_id + "_" + fotoMap.photo.id)
+																			return (
+																				<>
+																				<Card key={el.id  + "_" +  fotoMap.photo.id }>
+																				
+																				<div >
+																				<img src={fotoMap.photo.sizes[1].url}  />
+																				
+																				</div>
+																				
+																				
+																				
+																				
+																					</Card>
+																					</>
+
+																					
+																				
+																			)
+																		}
+																	}
+																	)
+
+																	}
+																	
+
+																
+															
+
+
+														</CardGrid>
+														<p><Link href={"https://vk.com/wall" + el.from_id +"_"+ el.id}>B исходное сообщение</Link></p>
+														</Group>
+													)
+												}
+											}
+										}
+									}
+									)}
+
+								</>}
+							<div className='scroll' id='response' />
+						</Div>
+					</Card>
+				</Group>
+
+			</div>
+		</div>
 
 	);
+
 
 }
 
